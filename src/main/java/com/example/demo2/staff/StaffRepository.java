5c8e84ca5ae3a86fdcd818b5ef852374404c1160
yuyanakao1121 <kaihatsu.yuya.nakao@gmail.com>
2023-06-20 15:18:43 +0900

    up

--- src/main/java/com/example/demo2/staff/StaffRepository.java
+++ src/main/java/com/example/demo2/staff/StaffRepository.java
@@ -4,9 +4,9 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface StaffRepository extends JpaRepository<Staff, Integer> {
-    List<Staff> findAll();
+    List<Staff> findAll();//データベース内のすべてのStaffエンティティを取得します。
 
-    void deleteById(int id);
-    Staff findById(int id);
+    void deleteById(int id);//指定されたIDに基づいてStaffエンティティを削除します。
+    Staff findById(int id);//指定されたIDに基づいてStaffエンティティを検索します。
 }
 
